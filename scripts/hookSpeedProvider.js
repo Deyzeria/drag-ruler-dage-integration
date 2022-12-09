1682b858e644c1e230efaf973b0a962f4a32e82b
Sinantrarion <vikbar199@gmail.com>
2022-12-09 20:32:08 +0200

    v2 Added Run

--- scripts/hookSpeedProvider.js
+++ scripts/hookSpeedProvider.js
@@ -1,8 +1,9 @@
 Hooks.once("dragRuler.ready", (SpeedProvider) => {
-    class MnMSpeedProvider extends SpeedProvider {
+    class AgeSpeedProvider extends SpeedProvider {
         get colors() {
             return [
-                {id: "Move", default: 0x04D40B}
+                {id: "Move", default: 0x04D40B},
+                {id: "Run", default: 0x04D40B}
             ]
         }
 
@@ -10,11 +11,12 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
             const baseSpeed = token.actor.system.speed.total;
 
             const ranges = [
-                {range: baseSpeed, color: "Move"}
+                {range: baseSpeed, color: "Move"},
+                {range: baseSpeed * 2, color: "Run"}
             ]
 
             return ranges
         }
     }
-    dragRuler.registerModule("drag-ruler-dage-integration", MnMSpeedProvider)
+    dragRuler.registerModule("drag-ruler-dage-integration", AgeSpeedProvider)
 })
